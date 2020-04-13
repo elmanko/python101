1f5bc30e4446e3f2949c3750aaa2dae6dfa1fd45
elmanko <elmankisimo@gmail.com>
2020-04-13 13:55:55 +0530

    caliz 1

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3.5
+print("hello world")
+
+var1 = 1 
+var2 = "1" + "lol".strip() #replace. or lstrip. or rstrip
+
+var4 = [1,"mames" , "mames2"]
+
+var5 = ()
+var6 = {}
+var7 = True
+var8 = 0.5
+
+var9 = list()
+var9.append(var1)
+'''
+comentario de bloque alv
+no 
+'''
+print(var1)
+print(var2)
+print(var8)
+print(var4)
+print(var5)
+print(var6)
+#print(var1+var7+var6+var7)
+print('no mames {},{},{}'.format(1,2,var6))
+print(var9)
